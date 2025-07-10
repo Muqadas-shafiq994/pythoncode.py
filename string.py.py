4ce1209d0f8537c21fe507459ccd4c8d51fe6a70
Muqadas-shafiq994 <ms.muqadas.shafiq@gmail.com>
2025-07-10 11:44:28 +0500

    Add files via upload

---
+++ string.py.py
@@ -0,0 +1,41 @@
+#string concatenation
+name="Muqadas "
+f_name="Muhammad Shafiq"
+print(name+f_name)
+# again
+n="maryam"
+m=" muqadas"
+print(n+m)
+
+#escape sequence
+#\n    for "new line"
+print("muqadas\nshafiq")
+print("hello\nworld")
+
+#\t   for   "tab space"
+print("muqadas\tshafiq")
+print("hello\tworld")
+
+#\\   for  backslash
+print("muqadas\\shafiq")
+print("hello\\world")
+
+#\'    for single quote
+print("muqadas\'shafiq")
+print("hello\'world")
+
+#\"   for   double quote
+print("muqadas\"shafiq")
+print("hello\"world")
+
+#\b  for remove backspace character
+print("muqadas\bshafiq")
+
+# input method
+
+b=int(input("enter a nunber:"))
+n=int(input("enter a number:"))
+print(b+n)
+print(b-n)
+print(b*n)
+print(b/n)
