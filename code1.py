d5c52a6a6a694f86f0c07b157d083dc88e1e783f
Muqadas-shafiq994 <ms.muqadas.shafiq@gmail.com>
2025-07-10 11:45:04 +0500

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,63 @@
+#title
+print("------Introduction of form-------\n")
+
+#Enter the name
+name=input("Enter name:")
+
+#Enter father name
+fname=input("Father name:")
+
+#age
+age=input("age :")
+
+#gender
+gender=input("Gender male/female:")
+
+#CNIC
+cnic=input("CNIC numder:")
+
+#religion
+religion=input("your religion:")
+
+#country 
+country=input("country name:")
+
+#phone no
+phone_no=input("phone number:")
+
+#email
+email=input("Email:")
+
+# something special about you
+special=input("special about you:")
+
+# Display
+
+print("------Introduction of form-------\n")
+print(f"Name          :{name}")
+print(f"Fname         :{fname}")
+print(f"Age           :{age}")
+print(f"Gender        :{gender}")
+print(f"CNIC          :{cnic}")
+print(f"Religion      :{religion}")
+print(f"Country name  :{country}")
+print(f"Phone no      :{phone_no}")
+print(f"Email         :{email}")
+print(f"Special       :{special}")
+print("\n thanks for filling ")
+
+#condition operator
+#equal
+a=12
+s=5
+print(a==s)
+#equal not
+print(a!=s)
+#geater then
+print(a>s)
+#less then
+print(a<s)
+#geater then equal 
+print(a>=s)
+#less then equal
+print(a<=s)
