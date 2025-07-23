a2a7ad7bfab8235881ef6ec9a6d464513300f695
Muqadas-shafiq994 <ms.muqadas.shafiq@gmail.com>
2025-07-23 12:03:56 +0500

    Add files via upload

---
+++ if_else_e.g.py
@@ -0,0 +1,12 @@
+#grade
+grade= int(input("enter your grade"))
+if grade>=90:
+    print("you got A grade")
+elif grade>=80:
+    print("you got B grade")
+elif grade>=60:
+    print("you got C grade")
+elif grade>=40:
+    print("you got D grade")
+else:
+    print("you are Fail")
